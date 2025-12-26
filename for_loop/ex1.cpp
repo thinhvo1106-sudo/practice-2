3e2bc5ea5bcfdab9fa61f2d58497d20941635835
huongvo1106 <56045114+huongvo1106@users.noreply.github.com>
2025-12-26 02:07:18 +0000

    ex1

--- for_loop/ex1.cpp
+++ for_loop/ex1.cpp
@@ -1,23 +1,25 @@
 #include<iostream>
+#include <string>
 using namespace std;
 
-bool findNum(int myArr[], int num);
+int countNum(int myArr[], int num);
 int main() {
-    int myArr[5] = {2,10,9,8,7};
-    int num = 5;
-    bool found = findNum(myArr, num);
-
-    if (found) {
-        cout << "Find the number" << endl;
-    } else {
-        cout << "Cannot find the number" << endl;
+    int myArr[8] = {2,10,9,8,7,2,4,6};
+    int num = 2;
+    int count = countNum(myArr, num);
+    if(count==0){
+        cout<<"The number does not exist"<<endl;
+    }else {
+        cout << "The number exist " << count << endl;
     }
+    
+
 
     return 0;
 }
 
 /** Find whether an number in the given array */
-bool findNum (int myArr[], int num) {
+bool findNum(int myArr[], int num) {
     for(int pos = 0; pos < 5; pos++) {
         if(num == myArr[pos]) {
             return true;
@@ -25,6 +27,20 @@ bool findNum (int myArr[], int num) {
     }
     return false;
 }
+
+/** Find whether an number in the given array. If yes, how many time does it exist? */
+int countNum(int myArr[], int num) {
+    int count = 0;
+    for( int pos=0;pos<8;pos++){
+        if(num == myArr[pos]) {
+            count++;
+        }
+    }
+    return count;
+}
+
+String 
+
 
 
 
