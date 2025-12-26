d9d5a0ceed98249325307c1912110da4dd2383f5
John Vo <thinhvo1106@gmail.com>
2025-12-26 16:45:24 +0000

    ex2

--- for_loop/ex2.cpp
+++ for_loop/ex2.cpp
@@ -1,17 +1,29 @@
 #include<iostream>
 #include <string>
+#include <cctype>   
 using namespace std;
 
 int findSpecialCharacter(string providedString);
 int main() {
-    string sentence = "Hello@123!";
+    string sentence = "Hello@123!"; 
     int count = findSpecialCharacter(sentence);
-
+    if(count==0){
+        cout<<"There are no special character";
+    }
+    else{
     cout << "There are " << count << " in the provided sentence." << endl;
-    return 0;
+    return 0;}
 }
 /**Find special character */
 int findSpecialCharacter(string providedString) {
-    //TODO
-    return 0;
+    char specialChar = '!';
+    int count = 0;
+    for(int pos=0; pos<10 ; pos++){
+        if(providedString[pos] == specialChar){
+            count++;
+        }
+        
+    }
+
+    return count;
 }
