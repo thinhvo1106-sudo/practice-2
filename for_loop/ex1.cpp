9baaa90f3ac39219203a6af9b7838093958901f8
huongvo1106 <56045114+huongvo1106@users.noreply.github.com>
2025-12-26 01:07:21 +0000

    ex1 question

---
+++ for_loop/ex1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+bool findNum(int myArr[], int num);
+int main() {
+    int myArr[5] = {2,10,9,8,7};
+    int num = 7;
+    bool exist = findNum(myArr, num);
+
+    if (exit) {
+        cout << "Find the number" << endl;
+    } else {
+        cout << "Cannot find the number" << endl;
+    }
+
+    return 0;
+}
+
+/** Find whether an number in the given array */
+bool findNum (int myArr[], int num) {
+    return 1;
+}
