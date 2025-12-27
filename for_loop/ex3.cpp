f4621c8fa3829bd048b54aa03eb2236a0308f7a4
huongvo1106 <56045114+huongvo1106@users.noreply.github.com>
2025-12-27 21:45:52 +0000

    ex3

---
+++ for_loop/ex3.cpp
@@ -0,0 +1,12 @@
+
+
+
+
+int main() {
+    int numList[] = {3, 8, 15, 22, 7, 14, 19, 26, 31, 4, 9, 18, 27, 10, 5, 16, 21, 12, 29, 6};
+
+    /**TODO: find sum of all even numbers in the list */
+
+    
+    return 0;
+}
