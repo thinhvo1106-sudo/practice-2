76330db77a1d8f2fdde0b6fe99135a5821896ff5
John Vo <thinhvo1106@gmail.com>
2025-12-27 22:35:57 +0000

    ex3

--- for_loop/ex3.cpp
+++ for_loop/ex3.cpp
@@ -1,12 +1,22 @@
-
-
-
-
+#include<iostream>
+#include <vector>
+using namespace std;
+int sumNumber(vector<int> myArr);
 int main() {
-    int numList[] = {3, 8, 15, 22, 7, 14, 19, 26, 31, 4, 9, 18, 27, 10, 5, 16, 21, 12, 29, 6};
+    vector<int> numList = {3, 8, 15, 22, 7, 14, 19, 26, 31, 4, 9, 18, 27, 10, 5, 16, 21, 12, 29, 6};
 
     /**TODO: find sum of all even numbers in the list */
-
+    cout<<"The sum of all even numbers is "<< sumNumber(numList)<<endl;
     
     return 0;
 }
+int sumNumber(vector<int> myArr){
+    int len = myArr.size();
+    int sumOfNum = 0;
+    for(int pos=0; pos < len; pos++){
+        if(myArr[pos] % 2 == 0 ){
+            sumOfNum = sumOfNum + myArr[pos];
+        }
+    }
+    return sumOfNum;
+ }
